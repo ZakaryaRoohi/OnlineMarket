0a2a36463fee6a95fc05bbda6670b2a319f9d5c2
ZakaryaRoohi <zakarya.roohi@gmail.com>
2021-04-12 13:08:52 +0430

    add SingleFragmentActivity Pattern

---
+++ app/src/main/java/com/example/onlinemarket/view/activity/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.example.onlinemarket.view.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.onlinemarket.R;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, createFragment())
+                    .commit();
+        }
+
+    }
+}
