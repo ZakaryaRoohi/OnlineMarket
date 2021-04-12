296c1735554af4ba43e2f65962239d85f830b963
ZakaryaRoohi <zakarya.roohi@gmail.com>
2021-04-12 12:08:41 +0430

    Generate POJO class from JSON as models

--- app/src/main/java/com/example/onlinemarket/view/activity/MainActivity.java
+++ app/src/main/java/com/example/onlinemarket/view/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.onlinemarket;
+package com.example.onlinemarket.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.onlinemarket.R;
 
 public class MainActivity extends AppCompatActivity {
 
