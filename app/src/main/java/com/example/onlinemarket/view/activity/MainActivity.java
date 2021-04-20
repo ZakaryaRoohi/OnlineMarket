4c0c65621b20baf0f31a42d6516116684aa33ffa
ZakaryaRoohi <zakarya.roohi@gmail.com>
2021-04-20 21:28:01 +0430

    add SnackBar to show  no internet

--- app/src/main/java/com/example/onlinemarket/view/activity/MainActivity.java
+++ app/src/main/java/com/example/onlinemarket/view/activity/MainActivity.java
@@ -9,15 +9,26 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+
 import com.example.onlinemarket.R;
 import com.example.onlinemarket.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
-public  class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mBinding;
+    private BroadcastReceiver mInternetState;
+
     public static Intent newIntent(Context context) {
         return new Intent(context, MainActivity.class);
     }
@@ -28,10 +39,53 @@ public  class MainActivity extends AppCompatActivity {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         NavController navController = Navigation.findNavController(this, R.id.main_nav_host);
-       mBinding.bottomNavigation.setSelectedItemId(R.id.nav_fragHome);
-        NavigationUI.setupWithNavController(mBinding.bottomNavigation,navController);
+        mBinding.bottomNavigation.setSelectedItemId(R.id.nav_fragHome);
+        NavigationUI.setupWithNavController(mBinding.bottomNavigation, navController);
+
+        mInternetState = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (!internetConnected(MainActivity.this)) {
+                    Snackbar snackbar = Snackbar.make(mBinding.getRoot(), R.string.no_internet, BaseTransientBottomBar.LENGTH_SHORT);
+                    View snackBarView = snackbar.getView();
+                    snackBarView.setTranslationY(-(convertDpToPixel(48, MainActivity.this)));
+                    snackbar.show();
+                }
+            }
+        };
+
     }
 
+    public static float convertDpToPixel(float dp, Context context) {
+        return dp * ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(mInternetState, new IntentFilter("android.net.com.CONNECTIVITY_CHANGE"));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mInternetState);
+    }
+
+
+    private boolean internetConnected(Context context) {
+        try {
+            ConnectivityManager connectivityManager =
+                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            return (networkInfo != null && networkInfo.isConnected());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 
 }
